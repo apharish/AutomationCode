59f4c0b4d144b4dfded127ab1a277268bc504faf
harishap050@gmail.com <harishap050@gmail.com>
2022-04-21 16:59:44 +0530

    Qa-Harish branch Changes

---
+++ AutomationProject/src/JavaStreamProject/DemoPojo.java
@@ -0,0 +1,22 @@
+package JavaStreamProject;
+
+public class DemoPojo {
+
+	private String courses;
+	private String courseInstructor;
+	
+	public String getCourses() {
+		return courses;
+	}
+	public void setCourses(String courses) {
+		this.courses = courses;
+	}
+	public String getCourseInstructor() {
+		return courseInstructor;
+	}
+	public void setCourseInstructor(String courseInstructor) {
+		this.courseInstructor = courseInstructor;
+	}
+	
+
+}

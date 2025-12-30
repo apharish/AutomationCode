f05856c814cdc88fccb06e3dd1892a7032cbe373
HARISH A P <harish.ap@hp.com>
2025-12-30 20:01:34 +0530

    Added Playwright assertion examples using expect.soft

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
@@ -12,9 +11,18 @@ export default defineConfig({
     timeout: 5000
   },
   reporter: 'html',
+  retries: 1,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-   browserName: 'chromium',
+    browserName: 'chromium',
+    headless: false,
+    viewport: null,
+
+    launchOptions: {
+      args: ['--start-maximized'] //to Maximize the browser window
+    },
+    trace: 'on-first-retry'
+
   },
 
 });
